19dc41afbf05097436ed71533898c8517258287f
mchen132 <Tom Chen>
2022-10-07 16:56:41 -0400

    Init commit

---
+++ src/main/java/com/TWCC/nyctodoservice/NycTodoServiceApplication.java
@@ -0,0 +1,13 @@
+package com.TWCC.nyctodoservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NycTodoServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NycTodoServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/TWCC/nyctodoservice/NycTodoServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.TWCC.nyctodoservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NycTodoServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
