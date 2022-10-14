b0845c20583bf684dc6718677b0af76f7bf63e1e
mchen132 <Tom Chen>
2022-10-14 21:30:30 +0000

    Add query by field

--- src/main/java/com/TWCC/controller/EventController.java
+++ src/main/java/com/TWCC/controller/EventController.java
@@ -1,9 +1,11 @@
 package com.TWCC.controller;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.TWCC.data.Event;
@@ -19,5 +21,15 @@ public class EventController {
     public List<Event> getEvents() {
         System.out.println("getEvents() is calls");
         return eventRepository.findAll();
+    }
+
+    @GetMapping("/events/{id}")
+    public Optional<Event> getEventsById(@PathVariable Integer id) {
+        return eventRepository.findById(id);
+    }
+
+    @GetMapping("/events/byaddress/{address}")
+    public List<Event> getEventsByAddress(@PathVariable String address) {
+        return eventRepository.findByAddress(address);
     }
 }
--- src/main/java/com/TWCC/data/EventRepository.java
+++ src/main/java/com/TWCC/data/EventRepository.java
@@ -1,9 +1,12 @@
 package com.TWCC.data;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface EventRepository extends JpaRepository<Event, Integer> {
     
+    public List<Event> findByAddress(String addr);
 }
