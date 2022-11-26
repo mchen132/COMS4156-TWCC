a96a017bb566f1c39cff853052c534b5c29e3678
Wesley Wei <ww2623@columbia.edu>
2022-11-26 15:39:14 -0500

    Added Event Statistic service methods calculating multiple averages with different field combinations - Updated corresponding fields in the EventStatistics class

--- service/src/main/java/com/TWCC/data/EventStatistics.java
+++ service/src/main/java/com/TWCC/data/EventStatistics.java
@@ -4,6 +4,10 @@ import java.util.Map;
 
 public class EventStatistics {
     private Map<String, Integer> numberOfEventsByCategory;
+    private int averageAgeLimitForEvents;
+    private float averageCostForEvents;
+    private Map<String, Float> averageCostOfEventsByCategory;
+    private Map<String, Integer> averageAgeLimitOfEventsByCategory;
 
     public Map<String, Integer> getNumberOfEventsByCategory() {
         return numberOfEventsByCategory;
@@ -11,6 +15,42 @@ public class EventStatistics {
 
     public EventStatistics setEventsByCategory(Map<String, Integer> numberOfEventsByCategory) {
         this.numberOfEventsByCategory = numberOfEventsByCategory;
+        return this;
+    }
+
+    public int getAverageAgeLimitForEvents() {
+        return averageAgeLimitForEvents;
+    }
+
+    public EventStatistics setAverageAgeLimitForEvents(int averageAgeLimitForEvents) {
+        this.averageAgeLimitForEvents = averageAgeLimitForEvents;
+        return this;
+    }
+
+    public float getAverageCostForEvents() {
+        return averageCostForEvents;
+    }
+
+    public EventStatistics setAverageCostForEvents(float averageCostForEvents) {
+        this.averageCostForEvents = averageCostForEvents;
+        return this;
+    }
+
+    public Map<String, Float> getAverageCostOfEventsByCategory() {
+        return averageCostOfEventsByCategory;
+    }
+
+    public EventStatistics setAverageCostOfEventsByCategory(Map<String, Float> averageCostOfEventsByCategory) {
+        this.averageCostOfEventsByCategory = averageCostOfEventsByCategory;
+        return this;
+    }
+
+    public Map<String, Integer> getAverageAgeLimitOfEventsByCategory() {
+        return averageAgeLimitOfEventsByCategory;
+    }
+
+    public EventStatistics setAverageAgeLimitOfEventsByCategory(Map<String, Integer> averageAgeLimitOfEventsByCategory) {
+        this.averageAgeLimitOfEventsByCategory = averageAgeLimitOfEventsByCategory;
         return this;
     }
 }
--- service/src/main/java/com/TWCC/service/EventStatisticService.java
+++ service/src/main/java/com/TWCC/service/EventStatisticService.java
@@ -10,7 +10,7 @@ public class EventStatisticService {
     /**
      * Gets number of events per event category
      * 
-     * @param events
+     * @param events list of events
      * @return Map<String, Integer> of number of events per category of events
      */
     public Map<String, Integer> getNumberOfEventsByCategory(List<Event> events) {
@@ -32,5 +32,107 @@ public class EventStatisticService {
         }
 
         return numberOfEventsByCategory;
+    }
+
+    /**
+     * Gets the average age limit out of all of the existing events
+     * 
+     * @param events list of events
+     * @return the average age limit for events
+     */
+    public int getAverageAgeLimitForEvents(List<Event> events) {
+        int totalNumberOfEvents = events.size();
+        int ageLimitTotal = 0;
+        for (Event event: events) {
+            ageLimitTotal += event.getAgeLimit();
+        }
+
+        return ageLimitTotal / totalNumberOfEvents;
+    }
+
+    /**
+     * Gets the average cost for all of the existing events
+     * 
+     * @param events list of events
+     * @return the average cost for events
+     */
+    public float getAverageCostForEvents(List<Event> events) {
+        int totalNumberOfEvents = events.size();
+        float costTotal = 0;
+        for (Event event: events) {
+            costTotal += event.getCost();
+        }
+
+        return costTotal / totalNumberOfEvents;
+    }
+
+    /**
+     * Gets the average cost of events per event category
+     * 
+     * @param events list of events
+     * @return the average cost of events per event category
+     */
+    public Map<String, Float> getAverageCostOfEventsByCategory(List<Event> events) {
+        Map<String, Float> averageCostOfEventsByCategory = new HashMap<String, Float>();
+        Map<String, Integer> numberOfEventsByCategory = new HashMap<String, Integer>();
+
+        for (Event event: events) {
+            String[] categories = event.getCategories().split(",");
+
+            for (int i = 0; i < categories.length; i++) {
+                String category = categories[i];
+
+                if (averageCostOfEventsByCategory.containsKey(category)) {
+                    averageCostOfEventsByCategory.put(category, averageCostOfEventsByCategory.get(category) + event.getCost());
+                    numberOfEventsByCategory.put(category, numberOfEventsByCategory.get(category) + 1);
+                } else {
+                    averageCostOfEventsByCategory.put(category, event.getCost());
+                    numberOfEventsByCategory.put(category, 1);
+                }
+            }
+        }
+
+        // Calculate average
+        for (String category: averageCostOfEventsByCategory.keySet()) {
+            Float averageCost = averageCostOfEventsByCategory.get(category) / numberOfEventsByCategory.get(category);
+            averageCostOfEventsByCategory.put(category, averageCost);
+        }
+
+        return averageCostOfEventsByCategory;
+    }
+
+    /**
+     * Gets the average age limit of events per event category
+     * 
+     * @param events list of events
+     * @return the average age limit of events per event category
+     */
+    public Map<String, Integer> getAverageAgeLimitOfEventsByCategory(List<Event> events) {
+        Map<String, Integer> averageAgeLimitOfEventsByCategory = new HashMap<String, Integer>();
+        Map<String, Integer> numberOfEventsByCategory = new HashMap<String, Integer>();
+
+        for (Event event: events) {
+            String[] categories = event.getCategories().split(",");
+
+            for (int i = 0; i < categories.length; i++) {
+                String category = categories[i];
+
+                if (averageAgeLimitOfEventsByCategory.containsKey(category)) {
+                    averageAgeLimitOfEventsByCategory.put(category, averageAgeLimitOfEventsByCategory.get(category) + event.getAgeLimit());
+                    numberOfEventsByCategory.put(category, numberOfEventsByCategory.get(category) + 1);
+                } else {
+                    averageAgeLimitOfEventsByCategory.put(category, event.getAgeLimit());
+                    numberOfEventsByCategory.put(category, 1);
+                }
+            }
+        }
+
+        // Calculate average
+        for (String category: averageAgeLimitOfEventsByCategory.keySet()) {
+            int averageAgeLimit = averageAgeLimitOfEventsByCategory.get(category) / numberOfEventsByCategory.get(category);
+            averageAgeLimitOfEventsByCategory.put(category, averageAgeLimit);
+        }
+
+        return averageAgeLimitOfEventsByCategory;
     }
 }
